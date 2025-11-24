ce747236374376f77ccb54faaa0a1720c001f15c
inhee <devforplayer@gmail.com>
2025-11-24 15:41:09 +0900

    v9.0 Oracle DB

--- Oracle_Source/src/DB_MAN.java
+++ Oracle_Source/src/DB_MAN.java
@@ -12,11 +12,10 @@ import java.sql.*;
 import java.io.*;
 
 public class DB_MAN {
-    String strDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    //String strURL = "jdbc:sqlserver://localhost:1433;DatabaseName=Automobile;";
-    String strURL = "jdbc:sqlserver://10.70.41.80:1433;DatabaseName=Automobile;";
-    String strEncrypt = "encrypt=true;trustServerCertificate=true;";
-    String strUser = "sa";
+    String strDriver = "oracle.jdbc.driver.OracleDriver";
+    //String strURL = "jdbc:oracle:thin:@10.70.41.80:1521";
+    String strURL = "jdbc:oracle:thin:@localhost:1521";
+    String strUser = "INHATC";
     String strPWD = "inha1958";
     
     Connection DB_con;
@@ -24,24 +23,35 @@ public class DB_MAN {
     ResultSet DB_rs;
     
     public void dbOpen() throws IOException {
+        HG_ErrorMessage();
+        
         try {
             Class.forName(strDriver);
-            strURL += strEncrypt;
             DB_con = DriverManager.getConnection(strURL, strUser, strPWD);
             DB_stmt = DB_con.createStatement();
         }
-        catch(Exception e) {
-            System.out.println("SQL Exception : " + e.getMessage());
+        catch (Exception e) {
+            System.out.println("SQLException : " + e.getMessage());
+            System.out.println("SQLException : " + e.toString());
         }
-    }    
+    }
     
     public void dbClose() throws IOException {
         try {
             DB_stmt.close();
             DB_con.close();
         }
-        catch (SQLException e) {
-            System.out.println("SQL Exception : " + e.getMessage());
+        catch(Exception e) {
+            System.out.println("SQLException : " + e.toString());
+        }
+    }
+    
+    public void HG_ErrorMessage() throws UnsupportedEncodingException {
+        try {
+            System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
